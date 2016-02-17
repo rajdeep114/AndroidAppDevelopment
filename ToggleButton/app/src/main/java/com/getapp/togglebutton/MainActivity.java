4a67c5ac1622044867d6e7722b4a9ca848b49fe3
Rajdeep Singh <rajdeep114@gmail.com>
2016-02-16 21:38:34 -0800

    Toggle Button

--- ToggleButton/app/src/main/java/com/getapp/togglebutton/MainActivity.java
+++ ToggleButton/app/src/main/java/com/getapp/togglebutton/MainActivity.java
@@ -2,12 +2,30 @@ package com.getapp.togglebutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.result);
+        text.setVisibility(View.INVISIBLE);         // Hides the textview
+    }
+
+    public void changeVibrateState(View view) {
+        boolean checked = ((ToggleButton) view).isChecked();
+        if(checked) {
+            text.setText("VibrateOn");
+            text.setVisibility(View.VISIBLE);
+        } else {
+            text.setText("VibrateOff");
+            text.setVisibility(View.INVISIBLE);
+        }
     }
 }
